e8d946a9cb808388ed36858726d081cff784c665
Akay <akaudeh@gmail.com>
2021-10-05 14:32:51 +0100

    modified HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,65 +13,92 @@
       Sign Up
     </div>
 
+    <div>
+      <div>More than just shorter links</div>
+      <div>
+        Build your brand’s recognition and get detailed insights 
+        on how your links are performing.
+      </div>
+      <button>Get Started</button>
+      <div></div>
+    </div>
 
-  More than just shorter links
+    <form>
+      <input type="text" placeholder="Shorten a link here...">
+      <button>Shorten It!</button>
+    </form>
 
-  Build your brand’s recognition and get detailed insights 
-  on how your links are performing.
+    <div>
+      <div>Advanced Statistics</div>
+      <div>
+        Track how your links are performing across the web with our 
+        advanced statistics dashboard.
+      </div>
+    </div>
+    <div>
+      <div>
+        <div><img src="" alt=""></div>
+        <h3>Brand Recognition</h3>
+        <p>
+          Boost your brand recognition with each click. Generic links don’t 
+          mean a thing. Branded links help instil confidence in your content.
+        </p>
+      </div>
 
-  Get Started
+      <div>
+        <div><img src="" alt=""></div>
+        <h3>Detailed Records</h3>
+        <p>
+          Gain insights into who is clicking your links. Knowing when and where 
+          people engage with your content helps inform better decisions.
+        </p>
+      </div>
+
+      <div>
+        <div><img src="" alt=""></div>
+        <h3>Fully Customizable</h3>
+        <p>
+          Improve brand awareness and content discoverability through customizable 
+          links, supercharging audience engagement.
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <div>Boost your links today</div>
+
+      <button>Get Started</button>
+    </div>
+
+    <div>
+      <div></div>
+      <div>
+        <p>Features</p>
+        <p>Link Shortening</p>
+        <p>Branded Links</p>
+        <p>Analytics</p>
+      </div>
+      <div>
+        <p>Resources</p>
+        <p>Blog</p>
+        <p>Developers</p>
+        <p>Support</p>
+      </div>
+      <div>
+        <p>Company</p>
+        <p>About</p>
+        <p>Our Team</p>
+        <p>Careers</p>
+        <p>Contact</p>
+      </div>
+
+      <div></div>
+    </div>
   
-  Shorten a link here...
-
-  Shorten It!
-
-  Advanced Statistics
-
-  Track how your links are performing across the web with our 
-  advanced statistics dashboard.
-
-  Brand Recognition
-
-  Boost your brand recognition with each click. Generic links don’t 
-  mean a thing. Branded links help instil confidence in your content.
-
-  Detailed Records
-
-  Gain insights into who is clicking your links. Knowing when and where 
-  people engage with your content helps inform better decisions.
-
-  Fully Customizable
-
-  Improve brand awareness and content discoverability through customizable 
-  links, supercharging audience engagement.
-
-  Boost your links today
-
-  Get Started
-
-  Features
-
-  Link Shortening
-  Branded Links
-  Analytics
-
-  Resources
-
-  Blog
-  Developers
-  Support
-
-  Company
-
-  About
-  Our Team
-  Careers
-  Contact
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
   </div>
 </template>
 

d7d71907062c1f672ff89ed4e927ff5391bd2b5f
Akay <akaudeh@gmail.com>
2021-10-06 01:22:31 +0100

    modified 3 files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
-    <div></div>
+    <div class="flex items-center justify-between mx-6 m-10 text-gray-600">
+      <div><img src="../assets/logo.svg" alt="Shortly logo"></div>
 
-    <div>
-      Features
-      Pricing
-      Resources
-    </div>
-
-    <div>
-      Login
-      Sign Up
-    </div>
-
-    <div>
-      <div>More than just shorter links</div>
       <div>
-        Build your brand’s recognition and get detailed insights 
-        on how your links are performing.
+        Features
+        Pricing
+        Resources
       </div>
-      <button>Get Started</button>
-      <div></div>
+
+      <div>
+        Login
+        Sign Up
+      </div>
+
+    </div>
+    <div class="flex flex-col-reverse">
+      <div class="mx-10 my-8">
+        <div class="text-4xl font-extrabold mb-3">More than just shorter links</div>
+        <div class="text-gray-400 mb-4">
+          Build your brand’s recognition and get detailed insights 
+          on how your links are performing.
+        </div>
+        <button class="rounded-full bg-gray-400 text-white font-bold px-7 py-3 cursor-pointer">Get Started</button>
+      </div>
+      <div class="ml-6 w-full h-120"><img src="../assets/illustration-working.svg" alt="" class="h-full"></div>
     </div>
 
     <form>
@@ -28,39 +32,41 @@
       <button>Shorten It!</button>
     </form>
 
-    <div>
-      <div>Advanced Statistics</div>
+    <div class="bg-gray-200 pt-16 py-10">
       <div>
-        Track how your links are performing across the web with our 
-        advanced statistics dashboard.
+        <div class="">Advanced Statistics</div>
+        <div>
+          Track how your links are performing across the web with our 
+          advanced statistics dashboard.
+        </div>
       </div>
-    </div>
-    <div>
       <div>
-        <div><img src="" alt=""></div>
-        <h3>Brand Recognition</h3>
-        <p>
-          Boost your brand recognition with each click. Generic links don’t 
-          mean a thing. Branded links help instil confidence in your content.
-        </p>
-      </div>
+        <div class="bg-white w-5/6 rounded m-auto pt-16 pb-8 px-6 mt-20">
+          <div><img src="" alt=""></div>
+          <h3 class="text-xl font-extrabold mb-4">Brand Recognition</h3>
+          <p class="text-gray-400 text-sm">
+            Boost your brand recognition with each click. Generic links don’t 
+            mean a thing. Branded links help instil confidence in your content.
+          </p>
+        </div>
 
-      <div>
-        <div><img src="" alt=""></div>
-        <h3>Detailed Records</h3>
-        <p>
-          Gain insights into who is clicking your links. Knowing when and where 
-          people engage with your content helps inform better decisions.
-        </p>
-      </div>
+        <div class="bg-white w-5/6 rounded m-auto pt-16 pb-8 px-6 mt-20">
+          <div><img src="" alt=""></div>
+          <h3 class="text-xl font-extrabold mb-4">Detailed Records</h3>
+          <p class="text-gray-400 text-sm">
+            Gain insights into who is clicking your links. Knowing when and where 
+            people engage with your content helps inform better decisions.
+          </p>
+        </div>
 
-      <div>
-        <div><img src="" alt=""></div>
-        <h3>Fully Customizable</h3>
-        <p>
-          Improve brand awareness and content discoverability through customizable 
-          links, supercharging audience engagement.
-        </p>
+        <div class="bg-white w-5/6 rounded m-auto pt-16 pb-8 px-6 mt-20">
+          <div><img src="" alt=""></div>
+          <h3 class="text-xl font-extrabold mb-4">Fully Customizable</h3>
+          <p class="text-gray-400 text-sm">
+            Improve brand awareness and content discoverability through customizable 
+            links, supercharging audience engagement.
+          </p>
+        </div>
       </div>
     </div>
 

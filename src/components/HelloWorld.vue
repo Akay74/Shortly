5e50a6b78772f1cf3e6e4906f2877d31b998b458
Akay <akaudeh@gmail.com>
2021-10-12 00:24:28 +0100

    made changes to the desktop view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="bg-white shadow-sm">
+    <nav class="bg-white">
 		<div class="max-w-6xl mx-auto px-4 py-8">
 			<div class="flex w-full justify-between relative">
 				<div class="flex space-x-7 w-full justify-between">
@@ -49,13 +49,13 @@
             </button>
           </div>
           <!-- mobile menu -->
-          <div v-show="mobileNav" class="mobile-menu absolute top-16 bg-violet-800 rounded-lg w-5/6 p-8">
+          <div v-show="mobileNav" class="mobile-menu absolute top-16 bg-violet-800 rounded-lg w-11/12 p-8 flex justify-center">
             <ul>
-              <li><a href="index.html" class="block px-2 py-4 text-cyan font-bold text-lg">Features</a></li>
-              <li><a href="#" class="block text-cyan px-2 py-4 font-bold text-lg hover:text-green-500 transition duration-300">Pricing</a></li>
-              <li class="mb-3 border-b-2"><a href="#" class="block text-cyan px-2 py-4 font-bold text-lg hover:text-green-500 transition duration-300">Resources</a></li>
-              <li><a href="" class="block py-4 px-2 text-cyan font-bold text-lg hover:text-green-500 transition duration-300">Login</a></li>
-              <button class=" block bg-cyan rounded-full px-6 py-2 text-white font-bold text-lg text-center w-full">Sign Up</button>
+              <li><a href="index.html" class="block px-2 py-4 text-white font-bold text-lg">Features</a></li>
+              <li><a href="#" class="block text-white px-2 py-4 font-bold text-lg hover:text-green-500 transition duration-300">Pricing</a></li>
+              <li class="mb-3 border-b-2"><a href="#" class="block text-white px-2 pt-4 pb-8 font-bold text-lg hover:text-green-500 transition duration-300">Resources</a></li>
+              <li><a href="" class="block py-4 px-2 text-white font-bold text-lg hover:text-green-500 transition duration-300">Login</a></li>
+              <button class=" block bg-cyan rounded-full mt-4 px-6 py-2 text-white font-bold text-lg text-center w-full">Sign Up</button>
               <li></li>
             </ul>
           </div>
@@ -63,7 +63,7 @@
 			</div>
 		</div>
 	</nav>
-    <div class="flex flex-col-reverse pb-32">
+    <div class="flex flex-col-reverse pb-32 lg:flex-row">
       <div class="mx-10 my-8">
         <div class="text-4xl font-extrabold mb-3">More than just shorter links</div>
         <div class="text-gray-400 mb-4">
@@ -76,9 +76,9 @@
     </div>
 
     <div class="bg-gray-200 pt-40 pb-12 relative">
-      <form class="h-auto w-5/6 rounded-md flex flex-col p-6 bg-blue-900 absolute -top-16">
-        <input type="text" placeholder="Shorten a link here..." class="w-full p-3 rounded mb-4">
-        <button class="w-full bg-cyan text-white font-bold rounded p-3">Shorten It!</button>
+      <form class="h-auto w-5/6 rounded-md flex flex-col p-6 bg-blue-900 absolute -top-16 lg:flex-row">
+        <input type="text" placeholder="Shorten a link here..." class="w-full p-3 rounded mb-4 lg:w-5/6">
+        <button class="w-full bg-cyan text-white font-bold rounded p-2.5">Shorten It!</button>
       </form>
       <div class="px-6 leading-10">
         <div class="text-2xl font-extrabold mb-4">Advanced Statistics</div>
@@ -91,7 +91,7 @@
         <div class="bg-white w-5/6 rounded m-auto pt-16 pb-8 px-6 mt-20 relative">
           <div class="m-auto w-24 bg-violet-800 rounded-full h-24 absolute -top-12 flex justify-center"><img class="self-center" src="../assets/icon-brand-recognition.svg" alt=""></div>
           <h3 class="text-xl font-extrabold mb-4">Brand Recognition</h3>
-          <p class="text-gray-400 text-sm">
+          <p class="text-gray-400 text-sm leading-6">
             Boost your brand recognition with each click. Generic links don’t 
             mean a thing. Branded links help instil confidence in your content.
           </p>
@@ -100,7 +100,7 @@
         <div class="bg-white w-5/6 rounded m-auto pt-16 pb-8 px-6 mt-20 relative">
           <div class="m-auto w-24 bg-violet-800 rounded-full h-24 absolute -top-12 flex justify-center"><img class="self-center" src="../assets/icon-detailed-records.svg" alt=""></div>
           <h3 class="text-xl font-extrabold mb-4">Detailed Records</h3>
-          <p class="text-gray-400 text-sm">
+          <p class="text-gray-400 text-sm leading-6">
             Gain insights into who is clicking your links. Knowing when and where 
             people engage with your content helps inform better decisions.
           </p>
@@ -109,7 +109,7 @@
         <div class="bg-white w-5/6 rounded m-auto pt-16 pb-8 px-6 mt-20 relative">
           <div class="m-auto w-24 bg-violet-800 rounded-full h-24 absolute -top-12 flex justify-center"><img class="self-center m-auto" src="../assets/icon-fully-customizable.svg" alt=""></div>
           <h3 class="text-xl font-extrabold mb-4">Fully Customizable</h3>
-          <p class="text-gray-400 text-sm">
+          <p class="text-gray-400 text-sm leading-6">
             Improve brand awareness and content discoverability through customizable 
             links, supercharging audience engagement.
           </p>
@@ -123,8 +123,8 @@
       <button class="rounded-full bg-gray-400 text-white font-bold px-7 py-3 cursor-pointer">Get Started</button>
     </div>
 
-    <div class="bg-violet-900 px-8 py-10">
-      <div class="mb-7 text-center"><img src="../assets/logo-white.svg" alt="Shortly logo"></div>
+    <div class="bg-violet-900 px-8 py-10 lg:flex lg:justify-between">
+      <div class="mb-7 text-center lg:w-1/3"><img src="../assets/logo-white.svg" alt="Shortly logo"></div>
       <div class="mb-7">
         <p class="text-white font-bold mb-4">Features</p>
         <p class="text-cyan mb-2">Link Shortening</p>
@@ -145,7 +145,7 @@
         <p class="text-cyan mb-2">Contact</p>
       </div>
 
-      <div class="flex w-3/5 items-center justify-between m-auto">
+      <div class="flex w-3/5 items-center justify-between m-auto lg:w-1/6">
         <div><img src="../assets/icon-facebook.svg" alt=""></div>
         <div><img src="../assets/icon-twitter.svg" alt=""></div>
         <div><img src="../assets/icon-pinterest.svg" alt=""></div>
@@ -153,7 +153,7 @@
       </div>
     </div>
   
-    <div class="attribution">
+    <div class="bg-violet-900 text-white">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
       Coded by <a href="https://github.com/Akay74" target="_blank">Akay</a>.
     </div>
